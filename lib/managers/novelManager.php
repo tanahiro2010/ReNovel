30f8468b24f64403b26e7e1917e4bdeae7288e35
YuitoAkatsuki <yuito@yuito.work>
2024-10-06 22:26:59 +0900

    :sparkles: NovelManagerクラスに小説を作成するメソッドを追加し、ユーザー存在確認メソッドを実装

--- lib/managers/novelManager.php
+++ lib/managers/novelManager.php
@@ -15,6 +15,35 @@ class NovelManager
         $this->DataBase = new JsonDB('../../db/novel.json');
     }
 
+    public function create($id, $title, $author, $genre, $status, $rating, $description): ?Novel
+    {
+        if ($this->exists(['id' => $id])) {
+            throw new Exception('Novel already exists.');
+        }
+
+        $data = [
+            'id' => $id,
+            'title' => $title,
+            'author' => $author,
+            'genre' => $genre,
+            'status' => $status,
+            'rating' => $rating,
+            'description' => $description,
+            'comments' => [],
+        ];
+        $this->DataBase->insert($data);
+        return new Novel($data);
+    }
+
+    /**
+     * Deletes the target data from the database.
+     *
+     * This method searches for the target data in the database using the provided
+     * target identifier and deletes all matching entries.
+     *
+     * @param array $target The target data to be deleted, containing an 'id' key.
+     * @return null Always returns null.
+     */
     public function delete($target): null
     {
         $idFilter = function ($callbackData) use ($target): bool {
@@ -30,5 +59,28 @@ class NovelManager
             }
         }
         return null;
+    }
+
+    /**
+     * Checks if a user exists in the JSON database.
+     *
+     * This method searches for a user in the JSON database located at '../db/user.json'
+     * by matching the provided data's 'id' field with the 'id' field in the database.
+     *
+     * @param array $data An associative array containing the user data to check. 
+     *                    It must include an 'id' key.
+     * @return bool Returns true if a user with the given 'id' exists in the database, 
+     *              otherwise returns false.
+     */
+    public function exists($data): bool
+    {
+        $idFilter = function ($callbackData) use ($data): bool {
+            if ($callbackData['id'] === $data['id']) {
+                return true;
+            }
+            return false;
+        };
+        $result = $this->DataBase->fetch($idFilter);
+        return count($result) > 0;
     }
 }
